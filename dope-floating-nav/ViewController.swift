f63bb59900f72917f521ddb6f0acf2c795aac3c5
Andrew Breckenridge <asbreckenridge@me.com>
2015-09-06 12:48:17 -0700

    import pop

--- dope-floating-nav/ViewController.swift
+++ dope-floating-nav/ViewController.swift
@@ -18,6 +18,24 @@ class ViewController: UIViewController {
     }
     var open = false
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let square = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        square.backgroundColor = UIColor.greenColor()
+        
+        view.addSubview(square)
+        
+        
+        let keyframeAnimation = CAKeyframeAnimation(keyPath: "position.x")
+        keyframeAnimation.values = [90, 0, 300, 200]
+        keyframeAnimation.keyTimes = [1, 1.6, 2, 2.7]
+        keyframeAnimation.duration = 4
+        
+        
+        square.layer.addAnimation(keyframeAnimation, forKey: "wiggle")
+    }
+    
 }
 
 
